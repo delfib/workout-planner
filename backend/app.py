bfc073c4ff049b96ea25221ec4710735345baede
Delfina Buil <delfinabuil2@gmail.com>
2026-06-24 11:10:02 -0300

    setup Flask backend with PostgreSQL and initial User model

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from config import Config
+from models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.route("/api/health")
 def health():
     return {"message": "API is running"}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
